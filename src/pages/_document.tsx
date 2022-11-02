adaad6164ae85f52a6cfbc2abecdcea7c045bb5f
Oliver Reznik <oliver@tunebat.com>
2022-11-02 17:23:27 -0500

    - updated favicon files

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -8,17 +8,32 @@ export default class MyDocument extends Document {
         return (
             <Html lang="en">
                 <Head>
-                    {/* PWA primary color */}
-                    <meta
-                        name="theme-color"
-                        // TODO pass primary theme color here
-                        // content={theme.palette.primary.main}
-                    />
-                    <link rel="shortcut icon" href="/favicon.ico" />
                     <link
-                        rel="stylesheet"
-                        href="https://fonts.googleapis.com/css?family=Roboto:300,400,500,700&display=swap"
+                        rel="apple-touch-icon"
+                        sizes="180x180"
+                        href="/apple-touch-icon.png"
                     />
+                    <link
+                        rel="icon"
+                        type="image/png"
+                        sizes="32x32"
+                        href="/favicon-32x32.png"
+                    />
+                    <link
+                        rel="icon"
+                        type="image/png"
+                        sizes="16x16"
+                        href="/favicon-16x16.png"
+                    />
+                    <link rel="manifest" href="/site.webmanifest" />
+                    <link
+                        rel="mask-icon"
+                        href="/safari-pinned-tab.svg"
+                        color="#5bbad5"
+                    />
+                    <meta name="msapplication-TileColor" content="#2e7d32" />
+                    <meta name="theme-color" content="#2e7d32" />
+
                     <meta name="emotion-insertion-point" content="" />
                     {(this.props as any).emotionStyleTags}
                 </Head>
