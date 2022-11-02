fcf0a0e99f4419d5ade369e515f59b98d7d5b471
Oliver Reznik <oliver@tunebat.com>
2022-11-02 11:32:28 -0500

    - reduced icon button size for SecureTextField

--- src/common/components/SecureTextField.tsx
+++ src/common/components/SecureTextField.tsx
@@ -24,6 +24,7 @@ export default function SecureTextField(props: TextFieldProps) {
                                     ? "hide password"
                                     : "show password"
                             }
+                            size="small"
                         >
                             {textIsVisible ? <VisibilityOff /> : <Visibility />}
                         </IconButton>
