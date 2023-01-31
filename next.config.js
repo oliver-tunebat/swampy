eef1f9e89ee3745b66f20491a8b27db56729e669
Oliver Reznik <oliver@tunebat.com>
2023-01-31 16:39:01 -0600

    - removed next.config.js rewrites

--- next.config.js
+++ next.config.js
@@ -1,16 +1,16 @@
 /** @type {import('next').NextConfig} */
 module.exports = {
     reactStrictMode: true,
-    async rewrites() {
-        return [
-            {
-                source: "/js/script.outbound-link.js",
-                destination: "https://plausible.io/js/script.js",
-            },
-            {
-                source: "/api/event",
-                destination: "https://plausible.io/api/event",
-            },
-        ];
-    },
+    // async rewrites() {
+    //     return [
+    //         {
+    //             source: "/js/script.outbound-link.js",
+    //             destination: "https://plausible.io/js/script.js",
+    //         },
+    //         {
+    //             source: "/api/event",
+    //             destination: "https://plausible.io/api/event",
+    //         },
+    //     ];
+    // },
 };
